0d65d7a3f6d9541f570bcfe46c13955d40f83ed2
zamaninemat9 <zamaninemat9@gmail.com>
2021-12-11 23:36:38 +0330

    Add crud for quiz model

--- classes/admin/quiz/quiz.js
+++ classes/admin/quiz/quiz.js
@@ -1,4 +1,5 @@
 const quizModel = require('./../../../model/admin/quiz');
+const prHlp = require('./../../../helpers/persian_date_helper');
 module.exports = new class login {
     index(req, res) {
         res.render('admin/quiz/index')
@@ -6,29 +7,65 @@ module.exports = new class login {
 
     async insert(req, res) {
         let {title,expireDate,startDate,description,catId} = req.body;
-        let body = {
-            title: req.body.title,
-            expireDate: req.body.expireDate,
-            startDate: req.body.startDate,
-            description: req.body.description,
-            catId: req.body.catId,
+        quizModel.create({
+            title,
+            expireDate,
+            startDate,
+            description,
+            catId,
             created: new Date()
-        }
-        await quizModel.create(body,(e) => {
-            if (e) {
-                return res.send({
-                    status: 500,
-                    description: err['500']
-                })
-            }
-            return res.send({
-                status:200,
-                description:err['200']
+        }, (e, d) => {
+            if (e) return res.send({
+                status: 500,
+                description: err['500']
+            });
+            res.send({
+                status: 200,
+                description: err['200']
             })
-        }).exec();
+        });
+    }
+
+    async update(req, res) {
+        let {title,expireDate,startDate,description,catId} = req.body;
+        let body = {
+            title,
+            expireDate,
+            startDate,
+            description,
+            catId,
+            updated: new Date()
+        }
+        await quizModel.findOneAndUpdate({_id: req.params.id}, body).exec();
         res.send({
-            status:200,
-            description:err['200']
+            status: 200,
+            description: err['200']
+        })
+    }
+
+    async delete(req,res) {
+        let data = await quizModel.findOne({_id: req.params.id}).exec();
+        if (!data) return res.send({
+            status: 402,
+            description: err['4022']
+        });
+        await quizModel.findByIdAndDelete({_id: req.params.id}).exec();
+        res.send({
+            status: 200,
+            description: err['200']
+        })
+    }
+
+    async get(req, res) {
+        let data = await quizModel.find().exec();
+        let arr = [];
+        let createAt = null;
+        for (let i in data) {
+            createAt = prHlp.get_def_date(data[i].created);
+        }
+        res.send({
+            status: 200,
+            data: arr
         })
     }
 };
